81394726ddd432d349547bf18525c85241e1f5e0
Fercasanov <fernando.casanova1992@outlook.com>
2024-03-17 20:06:45 -0300

    fix(requiment.txt): creacion de requirimentos

--- backend/src/helpers/authenticateJwt.py
+++ backend/src/helpers/authenticateJwt.py
@@ -11,9 +11,13 @@ def expire_data(days:int):
     new_date = now + timedelta(days)
     return new_date
 
+#generamos el token, lo enviamos al front, tanto el loggin y register, en ambos casos el token se guardara en una cookie
+
 def write_token(data):
     token = encode(payload={**data, "exp":expire_data(2)}, algorithm="HS256", key=secret_key)
     return token.encode("UTF-8")
+
+#este endpoint lo utilizamos en un use effect, para generar rutas privadas
 
 def validate_token(token, output=False):
     try:
